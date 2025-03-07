f1482f3003fbb575e9335746fcd6275977a63735
Gunturu_Chintu <107250168+GBEN10000@users.noreply.github.com>
2025-03-07 16:51:34 +0530

    Create Instanc.java

---
+++ JAVA-PROGRAMS/Instanc.java
@@ -0,0 +1,22 @@
+package com.codgnan.one;
+
+public class Instanc {
+	int id = 5434;
+	String name = "Chintu";
+	byte age = 20;
+	int salary = 100000000;
+	String Department = "Ai&Ds";
+
+	public static void main(String[] args) {
+	    Instanc n = new Instanc();
+	    
+	    System.out.println("ID: " + n.id);
+	    System.out.println("Name: " + n.name);
+	    System.out.println("Age: " + n.age);
+	    System.out.println("Department: " + n.Department);
+	    System.out.println("Salary: " + n.salary);
+		
+
+	}
+
+}
